c9dad39512936efbd3ffc72c5972b4774a687d6f
Salvador Espinosa <bochoespinosa@gmail.com>
2026-04-17 17:22:20 +0200

    fix for env variable rewrite

--- taskus_frontend_angular/set-env.js
+++ taskus_frontend_angular/set-env.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
 // Vercel will inject process.env.API_URL here during the build
-const targetPath = './src/environments/environment.prod.ts';
+const targetPath = './src/environments/environment.ts';
 const envConfigFile = `
 export const environment = {
   production: true,
-  apiUrl: '${process.env.API_URL || 'http://localhost:8080'}'
+  apiUrl: '${process.env.API_URL || 'http://localhost:3000'}'
 };
 `;
 
